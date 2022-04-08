ddc3941fa118cf78c6a8e450d12fd396da463a2b
Armin Weber <armin.weber@adesso.de>
2022-04-08 15:02:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taschenrechner/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.taschenrechner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Calculate(){
+        TextView res =findViewById(R.id.textViewResult);
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        Expression e = new ExpressionBuilder(calculation.getText().toString())
+                .build();
+        try {
+            double result = e.evaluate();
+            res.setText(Double.toString(result));
+        } catch (ArithmeticException arithmeticException) {
+            arithmeticException.printStackTrace();
+            res.setText("error");
+        }
+    }
+
+    public void onClick1(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"1");
+        Calculate();
+    }
+    public void onClick2(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"2");
+        Calculate();
+    }
+    public void onClick3(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"3");
+        Calculate();
+    }
+    public void onClick4(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"4");
+        Calculate();
+    }
+    public void onClick5(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"5");
+        Calculate();
+    }
+    public void onClick6(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"6");
+        Calculate();
+    }
+    public void onClick7(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"7");
+        Calculate();
+    }
+    public void onClick8(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"8");
+        Calculate();
+    }
+    public void onClick9(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"9");
+        Calculate();
+    }
+    public void onClick0(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"0");
+        Calculate();
+    }
+
+    public void onClickMultiply(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"*");
+    }
+    public void onClickDivide(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"/");
+    }
+    public void onClickMinus(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"-");
+    }
+    public void onClickPlus(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"+");
+    }
+    public void onClickDot(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+".");
+        Calculate();
+    }
+    public void onClick11(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+"(");
+        Calculate();
+    }
+    public void onClick12(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText(calculation.getText()+")");
+        Calculate();
+    }
+    public void onClickAC(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        calculation.setText("");
+    }
+    public void onClickDel(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        CharSequence str =calculation.getText();
+            if (str != null && str.length() > 0) {
+                str = str.toString().substring(0, str.length() - 1);
+            }
+        calculation.setText(str);
+        Calculate();
+    }
+
+//do math
+    public void onClickEquals(View view) {
+        TextView calculation =findViewById(R.id.textViewCalculation);
+        TextView res =findViewById(R.id.textViewResult);
+        calculation.setText(res.getText());
+        res.setText("");
+    }
+}
+
